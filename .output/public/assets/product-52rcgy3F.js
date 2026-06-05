b4596ccb53bca3c2db27bbb3f349047197810429
alina.mamenko <alina.mamenko@yalantis.net>
2026-06-05 13:57:23 +0300

    add functionality

---
+++ .output/public/assets/product-52rcgy3F.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./main-De2KMWX6.js";const n=()=>t.jsx(o,{});export{n as component};
---
+++ .output/server/chunks/_/product._productId-OPMXkX86.mjs
@@ -0,0 +1,31 @@
+import { a as createServerRpc, c as createServerFn } from "./server.mjs";
+import { p as products } from "./data-PA_hLshr.mjs";
+import "@tanstack/history";
+import "@tanstack/router-core/ssr/client";
+import "@tanstack/router-core";
+import "node:async_hooks";
+import "@tanstack/router-core/ssr/server";
+import "../../index.mjs";
+import "node:http";
+import "node:stream";
+import "node:https";
+import "node:http2";
+import "node:fs";
+import "node:url";
+import "node:path";
+import "tiny-invariant";
+import "seroval";
+import "react/jsx-runtime";
+import "@tanstack/react-router/ssr/server";
+import "@tanstack/react-router";
+const getProduct_createServerFn_handler = createServerRpc("02237705211f6fce5dafbd6be40b265cc093810759aabc102a7a61ff80e58dd5", (opts, signal) => getProduct.__executeServer(opts, signal));
+const getProduct = createServerFn({
+  method: "GET"
+}).inputValidator((data) => data).handler(getProduct_createServerFn_handler, async ({
+  data
+}) => {
+  return products.find((p) => p.id === data.productId) || null;
+});
+export {
+  getProduct_createServerFn_handler
+};
