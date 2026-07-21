67220aefdbbf65bcebc180f3570f5aaf5108383f
alina.mamenko <alina.mamenko@yalantis.net>
2026-07-21 22:20:44 +0300

    add functionality

---
+++ src/lib/contact.ts
@@ -0,0 +1,9 @@
+export const CONTACT_INFO = {
+  instagram: {
+    handle: "payka.jwlr",
+    url: "https://www.instagram.com/payka.jwlr/",
+  },
+  email: "mamenkooo@gmail.com",
+  phone: "+380 97 000 00 00",
+  phoneHref: "tel:+380970000000",
+} as const;
