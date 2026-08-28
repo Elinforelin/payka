bfafec989eb1f5cf189e8786dd16e0ff7a289d65
alina.mamenko <alina.mamenko@yalantis.net>
2026-08-28 18:18:34 +0300

    add functionality

---
+++ .output/public/assets/product-C_Aaku7Z.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./main-xOdJPNmr.js";const n=()=>t.jsx(o,{});export{n as component};
---
+++ .output/server/chunks/_/product-images-XBZXgYgm.mjs
@@ -0,0 +1,163 @@
+import { i as img } from "./me-3RncXd2G.mjs";
+const __vite_glob_0_0 = "/assets/2345413523454-Xt8wRga0.png";
+const __vite_glob_0_1 = "/assets/45875674576754-n8SRIrbi.png";
+const __vite_glob_0_2 = "/assets/521469718_18391966183189140_5158185447317376143_n-DV7w6U8k.jpg";
+const __vite_glob_0_3 = "/assets/5346354635465-0idztduK.png";
+const defaultImage = "/assets/619792737_18417153727189140_5984683189343682714_n-D8TUxlBa.jpg";
+const __vite_glob_0_5 = "/assets/7812354786123547-I0C1U1xA.png";
+const __vite_glob_0_6 = "/assets/5282821220627849158-DfogjQUX.jpg";
+const __vite_glob_0_7 = "/assets/5282821220627849159-DcVdvfL5.jpg";
+const __vite_glob_0_8 = "/assets/5282821220627849160-CfkbQknG.jpg";
+const __vite_glob_0_9 = "/assets/IMG_9086-D2cYClla.png";
+const __vite_glob_0_10 = "/assets/IMG_9100-IbpSBRDI.png";
+const __vite_glob_0_11 = "/assets/IMG_9115-Cfooyhpm.png";
+const __vite_glob_0_12 = "/assets/IMG_9118-BlWu087-.png";
+const __vite_glob_0_13 = "/assets/IMG_9133-CvFDJepk.png";
+const __vite_glob_0_14 = "/assets/amethyst-CymuyiJp.png";
+const __vite_glob_0_15 = "/assets/azure-9SbPZM9H.png";
+const __vite_glob_0_16 = "/assets/black-DrsCHDbS.png";
+const __vite_glob_0_17 = "/assets/blue-DvMUwwc9.png";
+const __vite_glob_0_18 = "/assets/champagne-CWhMv6xh.png";
+const __vite_glob_0_19 = "/assets/garnet-DMN6UBPM.png";
+const __vite_glob_0_20 = "/assets/green-CwbQ3KGP.png";
+const __vite_glob_0_21 = "/assets/lavender-B9iiRP9q.png";
+const __vite_glob_0_22 = "/assets/olive-Bb93XcxI.png";
+const __vite_glob_0_23 = "/assets/peridot-DlzG3Ivx.png";
+const __vite_glob_0_24 = "/assets/pink-DhKkeyrZ.png";
+const __vite_glob_0_25 = "/assets/red-AJPyvfwq.png";
+const __vite_glob_0_26 = "/assets/white-CVIsZzJV.png";
+const __vite_glob_0_27 = "/assets/yellow-79HrWYOE.png";
+const __vite_glob_0_28 = "/assets/5282821220627849167-B9vlzwNK.jpg";
+const __vite_glob_0_29 = "/assets/5282821220627849168-BHthtyBB.jpg";
+const __vite_glob_0_30 = "/assets/5282821220627849169-BMbkkhpa.jpg";
+const __vite_glob_0_31 = "/assets/5282821220627849170-DVP_w26p.jpg";
+const __vite_glob_0_32 = "/assets/IMG_4923-a4mg6Wg5.png";
+const __vite_glob_0_33 = "/assets/IMG_4935-B_49xHWQ.png";
+const __vite_glob_0_34 = "/assets/IMG_4939-IrMGZfHw.png";
+const __vite_glob_0_35 = "/assets/5267491911117774004-BfnEaEnc.png";
+const __vite_glob_0_36 = "/assets/IMG_4831-DBGRXD2-.png";
+const __vite_glob_0_37 = "/assets/IMG_4835-BCflxU0K.png";
+const __vite_glob_0_38 = "/assets/IMG_4837-BFFnsUx3.png";
+const __vite_glob_0_40 = "/assets/box-closed-BPGiG52J.png";
+const __vite_glob_0_41 = "/assets/box-open-If8SndzH.png";
+const __vite_glob_0_42 = "/assets/includes-LJbxRig8.png";
+const __vite_glob_0_43 = "/assets/5267491911117774016-DZWLj8IV.jpg";
+const __vite_glob_0_44 = "/assets/5267491911117774017-DzJjAsKi.jpg";
+const __vite_glob_0_45 = "/assets/5271827470149687881-BmJEuHgv.jpg";
+const __vite_glob_0_46 = "/assets/5271827470149687883-CHA2LveA.jpg";
+const __vite_glob_0_47 = "/assets/5271827470149687884-kQhSUqBS.jpg";
+const __vite_glob_0_48 = "/assets/IMG_1482-DKt_-8vN.png";
+const __vite_glob_0_49 = "/assets/IMG_1482-DKt_-8vN.png";
+const __vite_glob_0_50 = "/assets/IMG_1491-Dp01ZPVl.png";
+const __vite_glob_0_51 = "/assets/IMG_1491-Dp01ZPVl.png";
+const __vite_glob_0_52 = "/assets/IMG_1702-DR2LLU37.png";
+const __vite_glob_0_53 = "/assets/photo-86-CBbQNrru.png";
+const __vite_glob_0_54 = "/assets/photo-97-DEAisnHP.png";
+const __vite_glob_0_55 = "/assets/IMG_1444-CpvpEYhJ.png";
+const __vite_glob_0_56 = "/assets/IMG_1449--4b8vmxr.png";
+const __vite_glob_0_57 = "/assets/IMG_1462-D-twRZuc.png";
+const __vite_glob_0_58 = "/assets/IMG_1761-3WE9dH5Y.png";
+const __vite_glob_0_59 = "/assets/IMG_1742-tAUMdKu9.png";
+const __vite_glob_0_60 = "/assets/IMG_1742-tAUMdKu9.png";
+const __vite_glob_0_61 = "/assets/IMG_8727-BYf4eGRK.png";
+const __vite_glob_0_62 = "/assets/IMG_8730-pky_dXg6.png";
+const __vite_glob_0_63 = "/assets/IMG_8745-BybyIonF.png";
+const __vite_glob_0_64 = "/assets/IMG_8753-BkdoItUy.png";
+const __vite_glob_0_65 = "/assets/1000016899-D2hoKkIq.png";
+const __vite_glob_0_66 = "/assets/IMG_1339-R94EuZni.png";
+const __vite_glob_0_67 = "/assets/IMG_5741-TRQUx5rE.png";
+const __vite_glob_0_68 = "/assets/IMG_5742--bsMq_m9.png";
+const assetModules = /* @__PURE__ */ Object.assign({
+  "../assets/2345413523454.png": __vite_glob_0_0,
+  "../assets/45875674576754.png": __vite_glob_0_1,
+  "../assets/521469718_18391966183189140_5158185447317376143_n.jpg": __vite_glob_0_2,
+  "../assets/5346354635465.png": __vite_glob_0_3,
+  "../assets/619792737_18417153727189140_5984683189343682714_n.jpg": defaultImage,
+  "../assets/7812354786123547.png": __vite_glob_0_5,
+  "../assets/bubochki/5282821220627849158.jpg": __vite_glob_0_6,
+  "../assets/bubochki/5282821220627849159.jpg": __vite_glob_0_7,
+  "../assets/bubochki/5282821220627849160.jpg": __vite_glob_0_8,
+  "../assets/crumpledRing/IMG_9086.png": __vite_glob_0_9,
+  "../assets/crumpledRing/IMG_9100.png": __vite_glob_0_10,
+  "../assets/crumpledRing/IMG_9115.png": __vite_glob_0_11,
+  "../assets/crumpledRing/IMG_9118.png": __vite_glob_0_12,
+  "../assets/crumpledRing/IMG_9133.png": __vite_glob_0_13,
+  "../assets/cubicZirconiaColors/amethyst.png": __vite_glob_0_14,
+  "../assets/cubicZirconiaColors/azure.png": __vite_glob_0_15,
+  "../assets/cubicZirconiaColors/black.png": __vite_glob_0_16,
+  "../assets/cubicZirconiaColors/blue.png": __vite_glob_0_17,
+  "../assets/cubicZirconiaColors/champagne.png": __vite_glob_0_18,
+  "../assets/cubicZirconiaColors/garnet.png": __vite_glob_0_19,
+  "../assets/cubicZirconiaColors/green.png": __vite_glob_0_20,
+  "../assets/cubicZirconiaColors/lavender.png": __vite_glob_0_21,
+  "../assets/cubicZirconiaColors/olive.png": __vite_glob_0_22,
+  "../assets/cubicZirconiaColors/peridot.png": __vite_glob_0_23,
+  "../assets/cubicZirconiaColors/pink.png": __vite_glob_0_24,
+  "../assets/cubicZirconiaColors/red.png": __vite_glob_0_25,
+  "../assets/cubicZirconiaColors/white.png": __vite_glob_0_26,
+  "../assets/cubicZirconiaColors/yellow.png": __vite_glob_0_27,
+  "../assets/cup/5282821220627849167.jpg": __vite_glob_0_28,
+  "../assets/cup/5282821220627849168.jpg": __vite_glob_0_29,
+  "../assets/cup/5282821220627849169.jpg": __vite_glob_0_30,
+  "../assets/cup/5282821220627849170.jpg": __vite_glob_0_31,
+  "../assets/flower/IMG_4923.png": __vite_glob_0_32,
+  "../assets/flower/IMG_4935.png": __vite_glob_0_33,
+  "../assets/flower/IMG_4939.png": __vite_glob_0_34,
+  "../assets/lollypop/5267491911117774004.png": __vite_glob_0_35,
+  "../assets/lollypop/IMG_4831.png": __vite_glob_0_36,
+  "../assets/lollypop/IMG_4835.png": __vite_glob_0_37,
+  "../assets/lollypop/IMG_4837.png": __vite_glob_0_38,
+  "../assets/me.png": img,
+  "../assets/packaging/box-closed.png": __vite_glob_0_40,
+  "../assets/packaging/box-open.png": __vite_glob_0_41,
+  "../assets/packaging/includes.png": __vite_glob_0_42,
+  "../assets/plava/5267491911117774016.jpg": __vite_glob_0_43,
+  "../assets/plava/5267491911117774017.jpg": __vite_glob_0_44,
+  "../assets/plava/5271827470149687881.jpg": __vite_glob_0_45,
+  "../assets/plava/5271827470149687883.jpg": __vite_glob_0_46,
+  "../assets/plava/5271827470149687884.jpg": __vite_glob_0_47,
+  "../assets/roundWrinkledEarrings/IMG_1482 (1).png": __vite_glob_0_48,
+  "../assets/roundWrinkledEarrings/IMG_1482.png": __vite_glob_0_49,
+  "../assets/roundWrinkledEarrings/IMG_1491 (1).png": __vite_glob_0_50,
+  "../assets/roundWrinkledEarrings/IMG_1491.png": __vite_glob_0_51,
+  "../assets/smearedRing/IMG_1702.png": __vite_glob_0_52,
+  "../assets/smearedRing/photo-86.png": __vite_glob_0_53,
+  "../assets/smearedRing/photo-97.png": __vite_glob_0_54,
+  "../assets/vaseEarrings/IMG_1444.png": __vite_glob_0_55,
+  "../assets/vaseEarrings/IMG_1449.png": __vite_glob_0_56,
+  "../assets/vaseEarrings/IMG_1462.png": __vite_glob_0_57,
+  "../assets/vaseEarrings/IMG_1761.png": __vite_glob_0_58,
+  "../assets/waveRing/IMG_1742 (1).png": __vite_glob_0_59,
+  "../assets/waveRing/IMG_1742.png": __vite_glob_0_60,
+  "../assets/waveRing/IMG_8727.png": __vite_glob_0_61,
+  "../assets/waveRing/IMG_8730.png": __vite_glob_0_62,
+  "../assets/waveRing/IMG_8745.png": __vite_glob_0_63,
+  "../assets/waveRing/IMG_8753.png": __vite_glob_0_64,
+  "../assets/withPearl/1000016899.png": __vite_glob_0_65,
+  "../assets/withPearl/IMG_1339.png": __vite_glob_0_66,
+  "../assets/withPearl/IMG_5741.png": __vite_glob_0_67,
+  "../assets/withPearl/IMG_5742.png": __vite_glob_0_68
+});
+const assetsByPath = Object.fromEntries(
+  Object.entries(assetModules).map(([path, url]) => {
+    const normalizedPath = path.replace(/^\.\.\/assets/, "/assets");
+    return [normalizedPath, url];
+  })
+);
+const assetsByFilename = Object.fromEntries(
+  Object.entries(assetModules).map(([path, url]) => [path.split("/").pop(), url])
+);
+function resolveProductImageUrl(imageUrl) {
+  if (!imageUrl) return defaultImage;
+  if (assetsByPath[imageUrl]) {
+    return assetsByPath[imageUrl];
+  }
+  const filename = imageUrl.split("/").pop();
+  if (filename && assetsByFilename[filename]) {
+    return assetsByFilename[filename];
+  }
+  return defaultImage;
+}
+export {
+  resolveProductImageUrl as r
+};
